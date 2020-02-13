1a1817b85878969fcc92233fdbc1ee8e129f45f6
ameliasayaka <ameliasayaka@gmail.com>
2020-02-13 10:27:48 +0000

    Version 0.1
    Created XML file as test
    Began Writing code to load data
    
    To Do:
    Replace scene text and button text with text from XML

---
+++ EQ Trainer/Assets/Scripts/LoadScenario.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+[XmlRoot("Testing")]
+public class Scenarios
+{
+    [XmlArray("Scenarios")]
+    [XmlArrayItem("Scenario")]
+    public Scenario[] scenariosArray;
+
+    public static Scenarios Load(string path)
+    {
+        var serializer = new XmlSerializer(typeof(Scenarios));
+        using (var stream = new FileStream(path, FileMode.Open))
+        {
+            return serializer.Deserialize(stream) as Scenarios;
+        }
+    }
+
+}
+public class Scenario
+{
+    [XmlAttribute("category")]
+    public string category;
+
+    public string text;
+
+    [XmlArray("Options")]
+    [XmlArrayItem("option")]
+    public List<Option> Options = new List<Option>();
+
+
+}
+public class Option
+{
+    public string text;
+    public int score;
+}
+
---
+++ EQ Trainer/Assets/Scripts/ScenarioHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScenarioHandler : MonoBehaviour
+{
+    private string fileName = "testScenarioData.xml";
+    [SerializeField]
+    private Text scenarioText;
+    [SerializeField]
+    private Text[] buttonList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadScenario();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadScenario()
+    {
+
+    }
+}
